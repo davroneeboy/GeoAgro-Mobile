fe666f01b8ee262fbfd0bbbe4d45ece207d2679e
dtukhtamuratov <davron.tukhtamuratov.1808@gmail.com>
2025-05-21 10:38:55 +0500

    luxa

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL1 = "http://142.93.221.110/";
-export const API_BASE_URL2 = "http://142.93.221.110/";
+export const API_BASE_URL1 = "https://luxa.uz/";
+export const API_BASE_URL2 = "https://luxa.uz/";
 // export const API_BASE_URL2 = "https://uzagrosanoat.uz/";
 export const GOOGLE_API_KEY = "AIzaSyCg0jBupD18sGdEe-8ygynVUiSwXS34_CI";
